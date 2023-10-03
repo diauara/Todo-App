d21e66f7ffa17d4446c10963bb279a10b9bb5501
Diamond Akinro <akinrodiau25@gmail.com>
2023-10-03 11:30:07 +0100

    changed the way the delete works

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons'
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, pressHandler }) {
-
-    return(
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <View style={styles.item}>
-                <MaterialIcons name='delete' size={18} color='#333' />
-                <Text style={styles.itemText}>{item.text}</Text>
-            </View>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity>
+      <View style={styles.item}>
+        <MaterialIcons
+          name="delete"
+          size={18}
+          color="#333"
+          onPress={() => pressHandler(item.key)}
+        />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: "#bbb",
-        borderWidth: 1,
-        borderStyle: "dashed",
-        borderRadius: 10,
-        flexDirection: "row",
-    },
-    itemText: {
-        marginLeft: 10,
-    },
-})
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+    flexDirection: "row",
+  },
+  itemText: {
+    marginLeft: 10,
+  },
+});
